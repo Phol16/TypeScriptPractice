92a1b130ff4a7021e4eea1d2cd8aa32911f62557
Phol <Pholibertlim@gmail.com>
2023-02-23 15:50:05 +0800

    Feat: classes practice TS

---
+++ sampleProj/build/js/Classes.js
@@ -0,0 +1,104 @@
+"use strict";
+class Code {
+    constructor(name, age, language) {
+        this.name = name;
+        this.age = age;
+        this.language = language;
+    }
+}
+//with visibility modifier
+class TheCode {
+    // secondLang !: string //propety that will not instanciate right away by using '!'(assertion)
+    //private vs protected //both can't be access outside of the class but.
+    // (private can only be access by that specific class)
+    // (protected other class can access it as long as its extended from the source class)
+    constructor(name, language, age, programmingLang = 'javascript') {
+        this.name = name;
+        this.language = language;
+        this.age = age;
+        this.programmingLang = programmingLang;
+    }
+    getAge() {
+        return `hello ${this.age}`;
+    }
+}
+const Phol = new TheCode('phol', 'tagalog', 12, 'typescript');
+console.log(Phol.getAge());
+// console.log( Phol.age); // gives error cause its private
+// console.log( Phol.programmingLang); // gives error cause its protected
+class WebDev extends TheCode {
+    constructor(computer, name, language, age) {
+        super(name, language, age);
+        this.computer = computer;
+    }
+    //gives error casue age is private
+    // public getAge(){
+    //   return `I am ${this.age}`
+    // }
+    //didn't get an error cause programmingLang is protected and can be access
+    getProgrammingLang() {
+        return `I wrtie ${this.programmingLang}`; //has a defaultvalue
+    }
+}
+const Bert = new WebDev('laptop', 'phol', 'tagalog', 45);
+console.log(Bert.computer);
+console.log(Bert.name);
+console.log(Bert.getProgrammingLang());
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+const Pholi = new Guitarist('phol', 'guitar');
+console.log(Pholi.play('practice'));
+/////////////////////////////////////////////////////////////////
+//static implies directly on the class (??)
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const John = new Peeps('John');
+const Amy = new Peeps('Amy');
+const David = new Peeps('David');
+const Joe = new Peeps('Joe');
+console.log(Peeps.getCount());
+console.log(John.id);
+console.log(David.id);
+//getters and setter
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    // readonly it gets data (getter)
+    get data() {
+        return this.dataState;
+    }
+    //writes data (setter)
+    set data(value) {
+        //.every() checks every string if it satisfy it will return true else will return false
+        if (Array.isArray(value) && value.every((el) => typeof el === 'string')) {
+            this.dataState = value;
+            return;
+        }
+        else {
+            throw new Error('not an array of string');
+        }
+    }
+}
+const myBand = new Bands();
+myBand.data = ['Neil Yound', 'Led']; //setter
+console.log(myBand.data); //getter
+myBand.data = [...myBand.data, 'hello'];
+console.log(myBand.data);
+// myBand.data = [452,2342]; // gives error because setters of data accepts an array of string
+// console.log(myBand.data);
